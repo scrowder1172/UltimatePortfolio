c06f8e15ff428e26695d0005b9cc83e4251dd191
Scott Crowder <scrowder@gmail.com>
2024-11-13 12:09:15 -0600

    Initial Commit

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// File: ContentView.swift
+// Project: UltimatePortfolio
+// 
+// Created by SCOTT CROWDER on 11/13/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,20 @@
+//
+// File: UltimatePortfolioApp.swift
+// Project: UltimatePortfolio
+// 
+// Created by SCOTT CROWDER on 11/13/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
