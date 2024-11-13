3d70d30e19ec4fc4ea49e16edb36b151f523aa26
Scott Crowder <scrowder@gmail.com>
2024-11-13 13:39:22 -0600

    Configured core data and icloud

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -12,9 +12,14 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    
+    @StateObject var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
