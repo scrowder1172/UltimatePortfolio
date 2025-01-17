225397390f87455a4dcd42b3f83d0d316aae4671
Scott Crowder <scrowder@gmail.com>
2025-01-17 11:17:42 -0600

    Create new issues

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -213,4 +213,19 @@ class DataController: ObservableObject {
         let allIssues = (try? container.viewContext.fetch(request)) ?? []
         return allIssues.sorted()
     }
+    
+    func newIssue() {
+        let issue = Issue(context: container.viewContext)
+        issue.title = "New Issue"
+        issue.creationDate = .now
+        issue.priority = 1
+        
+        if let tag = selectedFilter?.tag {
+            issue.addToTags(tag)
+        }
+        
+        save()
+        
+        selectedIssue = issue
+    }
 }
