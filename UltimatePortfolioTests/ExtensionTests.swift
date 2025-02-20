842647634e6f5d85a4fa346c6fca059363b9c5b2
Scott Crowder <scrowder@gmail.com>
2025-02-20 09:14:47 -0600

    add additional tests

--- UltimatePortfolioTests/ExtensionTests.swift
+++ UltimatePortfolioTests/ExtensionTests.swift
@@ -132,5 +132,19 @@ final class ExtensionTests: BaseTestCase {
         let awards = Bundle.main.decode("Awards.json", as: [Award].self)
         XCTAssertFalse(awards.isEmpty, "Awards.json should decode to a non-empty array.")
     }
+    
+    func testDecodingString() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode("DecodableString.json", as: String.self)
+        XCTAssertEqual(data, "Never ask a starfish for directions.", "The string must match DecodableString.json")
+    }
+    
+    func testDecodingDictionary() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode("DecodableDictionary.json", as: [String: Int].self)
+        
+        XCTAssertEqual(data.count, 3, "There should be three items decoded from DecodableDictionary.json")
+        XCTAssertEqual(data["One"], 1, "The dictionary should contain the value 1 for the key One.")
+    }
 
 }
